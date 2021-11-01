30cb3d99220b7d9e50f2e1d752024df7cf301f3d
alexm <a.matcov@student.utwete.nl>
2021-11-01 12:02:40 +0100

    Added requirement

--- dashboard.py
+++ dashboard.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib
 
 st.title("OPTION PRICE DASHBOARD")
 st.write("by [Group 2](https://canvas.utwente.nl/groups/92289/users)")
