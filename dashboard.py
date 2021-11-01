25db25118f23d259dd2734a9c2dc1e6e25cc0855
alexm <a.matcov@student.utwete.nl>
2021-11-01 11:54:28 +0100

    Change import name

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib as plt
 
 st.title("OPTION PRICE DASHBOARD")
 st.write("by [Group 2](https://canvas.utwente.nl/groups/92289/users)")
